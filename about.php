2f05c52d703e046833702a0f8b73ae0fdb7ababe
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2025-06-24 15:22:30 +0200

    Home + About
    Home + about me  pages are made. Header + footer also along with begginings for the data of my projects

--- about.php
+++ about.php
@@ -1,7 +1,35 @@
 <?php
+$pageTitle = 'About Me | Julia Brouwer';
 include_once 'header.php';
 ?>
+<link rel="stylesheet" href="assets/css/base.css">
+<link rel="stylesheet" href="assets/css/about.css">
 
+<main class="about-main">
+    <section class="about-content">
+        <div class="about-left">
+            <div class="about-photo-placeholder">Foto hier</div>
+        </div>
+        <div class="about-right">
+            <h1 class="about-heading">Over mij</h1>
+            <p class="about-bio">
+                Hoi! Ik ben Julia Brouwer, een enthousiaste web developer in opleiding aan het Grafisch Lyceum Utrecht. Ik hou van het maken van mooie, functionele websites en het leren van nieuwe technologieën. <br><br>
+            </p>
+            <div class="about-skills">
+                <h2 class="skills-heading">Skills & Technologieën</h2>
+                <ul class="skills-list">
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>MySQL</li>
+                    <li>Python</li>
+                    <li>Figma</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?php
 include_once 'footer.php';
--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
+$pageTitle = 'Home | Julia Brouwer';
 include_once 'header.php';
 ?>
+<link rel="stylesheet" href="assets/css/base.css">
+<link rel="stylesheet" href="assets/css/home.css">
 
+<main class="home-main">
+    <section class="home-content">
+        <div class="home-left">
+            <div class="home-role">Web Developer</div>
+            <h1 class="home-name">Julia Brouwer</h1>
+            <div class="home-buttons">
+                <a href="about.php" class="btn btn-outline">Over mij</a>
+                <a href="projects.php" class="btn btn-accent">Mijn werk</a>
+            </div>
+        </div>
+        <div class="home-right">
+            <p>Mijn naam is Julia Brouwer, en ik ben een 2e jaars student op het Grafisch Lyceum Utrecht. <br> Ik zit in de opleiding web development voltijd.<br><br>
+            Klik vooral om een indruk te krijgen over mijn en het werk dat ik maak!</p>
+        </div>
+    </section>
+</main>
 
 <?php
 include_once 'footer.php';
