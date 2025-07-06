fb562159cac03b2cf4c25306717175a10356647a
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2025-07-06 16:53:39 +0200

    favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : 'Julia Brouwer | Portfolio'; ?></title>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;700&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/avif" href="assets/img/pinkStar.avif">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
